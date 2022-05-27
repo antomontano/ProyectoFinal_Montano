8d41836228d97da9db531d1bdd9e35f102ac0853
Antonela <antomontano2@gmail.com>
2022-05-27 17:48:14 -0300

    Tercer commit

--- AppUniversidad/forms.py
+++ AppUniversidad/forms.py
@@ -4,4 +4,3 @@ from django import forms
 class inscribirmeFinales(forms.Form):
     Materia= forms.CharField(max_length=1)
     Mesa= forms.IntegerField()
-    
--- AppUniversidad/views.py
+++ AppUniversidad/views.py
@@ -1,7 +1,11 @@
+from django import http
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse
 from AppUniversidad.forms import *
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 def inicio(request):
@@ -39,3 +43,46 @@ def EliminarInscripcion(request, id):
     inscripcion.delete()
     inscripciones=Inscripcion.objects.all()
     return render(request, 'AppUniversidad/inscripciones.html', {'inscripciones':inscripciones})
+
+#-----------------LOGIN-----------------------------
+def Login_request(request):
+    if request.method == "POST":
+        formulario = AuthenticationForm(request=request, data=request.POST)
+       
+        if formulario.is_valid():
+            usuario = formulario.cleaned_data.get('username')
+            clave = formulario.cleaned_data.get('password')
+
+
+            user=authenticate(username=usuario, password=clave)
+            
+            if user is not None:
+                login(request)
+                return render(request, 'AppUniversidad/principal.html')
+            
+            else:
+                return render(request, 'AppUniversidad/login.html', {'Mensaje': 'Usuario incorrecto, intente nuevamente'})
+      
+        else:
+            return render(request, 'AppUniversidad/login.html', {'Mensaje': 'Formulario inválido'})
+    
+    else:
+        formulario=AuthenticationForm()
+        return render(request, 'AppUniversidad/login.html', {'formulario': formulario})
+
+
+#---------------REGISTRO-----------------
+
+def registrarme(request):
+    if request.method == "POST":
+        formulario = UserCreationForm(request.POST)
+        
+        if formulario.is_valid():
+            username=formulario.cleaned_data['username']
+            formulario.save()
+            return render(request, 'AppUniversidad/login.html', {'Mensaje': f'Usuario {username} creado exitosamente'})
+        else:
+            return render(request, 'AppUniversidad/login.html', {'Mensaje': 'No se pudo crear el usuario'})
+    else:
+        formulario = UserCreationForm()
+        return render(request, 'AppUniversidad/registrarme.html', {'formulario': formulario})
